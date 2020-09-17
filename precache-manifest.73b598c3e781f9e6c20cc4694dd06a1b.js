7fd022364c3708924d31268c87c0900ade88b450
Shilpa Hemaraj <shilpahemaraj@Shilpas-MacBook-Pro.local>
2020-09-16 23:07:51 -0700

    Updates

---
+++ precache-manifest.73b598c3e781f9e6c20cc4694dd06a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271f280abc99705d9bed918e6657b977",
+    "url": "/hangman-using-reactjs/index.html"
+  },
+  {
+    "revision": "19d0ec28c1fe30a9b6c7",
+    "url": "/hangman-using-reactjs/static/css/main.bab13ed4.chunk.css"
+  },
+  {
+    "revision": "3a75e9abe1c5688fbf88",
+    "url": "/hangman-using-reactjs/static/js/2.9a1198f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-using-reactjs/static/js/2.9a1198f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d0ec28c1fe30a9b6c7",
+    "url": "/hangman-using-reactjs/static/js/main.b9ca5cc8.chunk.js"
+  },
+  {
+    "revision": "c000aa789cbe0118b659",
+    "url": "/hangman-using-reactjs/static/js/runtime-main.fd575f1c.js"
+  }
+]);
